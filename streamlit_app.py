a4f1feea5793a34d64d8a16ce3ee7df02ba630ff
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-26 20:44:46 +0530

    Update streamlit_app.py
    Adding cooments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Dinner')
+streamlit.text('Omega')
+streamlit.text('pancake')
+streamlit.text('pastery')
