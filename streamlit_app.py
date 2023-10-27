52ea397b78f1ce4aa9da05d0964f46eea1e0775b
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-27 19:00:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas
 import streamlit
-import requests
 fruit_choice=streamlit.text_input('what is your favourite fruit','Kiwi')
 streamlit.write('Use entered',fruit_choice)
-fruitvicy_response=requests.get("https://fruityvice.com/api/fruit" +fruit_choice)
+import requests
+fruitvicy_response=requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
