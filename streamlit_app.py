0193f8d0a3147dddac798eb0f4e8a1dbd7d73b6e
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-27 17:11:20 +0530

    Update streamlit_app.py
    Adding Api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import pandas
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-my_text_file=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_text_file=my_text_file.set_index('Fruit')
-Fruit_Selected=streamlit.multiselect("Pick Some fruits:", list(my_text_file.index),['Avocado','Strawberries'])
-Fruit_to_show=my_text_file.loc[Fruit_Selected]
-streamlit.dataframe(Fruit_to_show)
-
-
+import requests
+fruitvicy_response=requests.get('https://fruityvice.com/api/fruit/watermelon')
+streamlit.text(fruitvicy_response)
