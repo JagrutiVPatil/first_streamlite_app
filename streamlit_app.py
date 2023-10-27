bd9a20f4194df25075dc33e1dd2acf1cbc1dfd3f
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-27 12:38:17 +0530

    Update streamlit_app.py
    Adding few More Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 my_text_file=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_text_file=my_text_file.set_index('Fruit')
 streamlit.multiselect("Pick Some fruits:", list(my_text_file.index))
 streamlit.dataframe(my_text_file)
 
