887351cf4bd6180f6df2a4f2b04577b347458695
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-27 12:32:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import pandas
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-my_text_file=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_text_file)
+
