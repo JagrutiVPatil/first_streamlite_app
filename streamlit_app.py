adfd965be6dae032c4ce9dc80c81c3e8ccec0343
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-27 17:22:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import requests
 streamlit.header('Fruitvicy header')
 fruitvicy_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruitvicy_response)
-fruitvicy_normalize=pandas.json_normalize(fruivicy_response.json())
+fruitvicy_normalize=pandas.json_normalize(fruitvicy_response.json())
 streamlit.dataframe(fruitvicy_normalize)
 
