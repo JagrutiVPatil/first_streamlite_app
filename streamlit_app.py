fbaa40cc54d59cdc56bf8cb744db59ce2e2f9541
JagrutiVPatil <149088921+JagrutiVPatil@users.noreply.github.com>
2023-10-28 17:29:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Parents New Healthy Dinner')
 my_text_file=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_text_file=my_text_file.set_index('Fruit')
